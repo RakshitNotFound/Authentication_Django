a556bce350430762f9f93e0b406bbdbf8745e2ec
Raksh <99888381+RakshitNotFound@users.noreply.github.com>
2024-07-17 00:31:39 +0530

    Add files via upload

---
+++ authentication/crm/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.homepage, name=""),
+    
+    path('register', views.register, name="register"),
+    
+    path('my-login', views.my_login, name="my-login"),
+    
+    path('dashboard', views.dashboard, name="dashboard"),
+    
+]
